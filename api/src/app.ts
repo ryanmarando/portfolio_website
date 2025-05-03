adba0813d1656fa09d8e2be90acf6602e360f2fd
Ryan Marando <marandoryan@gmail.com>
2025-05-03 19:26:45 -0400

    deployed timezone

--- api/src/app.ts
+++ api/src/app.ts
@@ -7,7 +7,7 @@ import wbgtRouter from "./routes/wbgt.js";
 import modelTrender from "./routes/modelTrender.js";
 
 const app = express();
-const port = 3001;
+const port = 10000;
 
 const corsOptions = {
     origin: [
