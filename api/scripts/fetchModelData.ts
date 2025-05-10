bb666de7bb8a8c9d673bb9eb58ddb4cfa1cc786d
Ryan Marando <marandoryan@gmail.com>
2025-05-10 11:23:26 -0400

    changed cron job logic

--- api/scripts/fetchModelData.ts
+++ api/scripts/fetchModelData.ts
@@ -1,11 +1,10 @@
 // scripts/fetchModelData.js
-import axios from "axios";
 import { API_URL } from "../src/lib/alertdict.js";
 
 const run = async () => {
   try {
-    const res = await axios.get(`${API_URL}/modelTrender`);
-    console.log("Triggered modelTrender:", res.data);
+    const res = await fetch(`${API_URL}/modelTrender`);
+    console.log("Triggered modelTrender");
   } catch (err) {
     console.error("Error triggering modelTrender:", err);
     process.exit(1);
